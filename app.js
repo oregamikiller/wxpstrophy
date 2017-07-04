1605c86a929fabc208811c2757dc01eaecfcf80d
oregami <oregami@163.com>
2017-07-05 00:39:30 +0800

    init wx

---
+++ app.js
@@ -0,0 +1,47 @@
+//app.js
+App({
+  onLaunch: function() {
+    //调用API从本地缓存中获取数据
+    var that = this
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+    wx.request({
+      url: 'https://semidream.com/trophydata/?platForm=ps4', //仅为示例，并非真实的接口地址
+      data: {
+        x: '',
+        y: ''
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data)  
+        that.setData({
+          list: res.data
+        })
+      }
+    })
+  },
+
+  getUserInfo: function(cb) {
+    var that = this
+    if (this.globalData.userInfo) {
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    } else {
+      //调用登录接口
+      wx.getUserInfo({
+        withCredentials: false,
+        success: function(res) {
+          that.globalData.userInfo = res.userInfo
+          typeof cb == "function" && cb(that.globalData.userInfo)
+        }
+      })
+    }
+  },
+
+  globalData: {
+    userInfo: null,
+    list:[]
+  }
+})
