c3bac8618398f7384350fedc6f31edb0ef186f7e
oregami <oregami@163.com>
2017-07-07 18:25:44 +0800

    add pull refresh

--- pages/index/index.js
+++ pages/index/index.js
@@ -34,5 +34,21 @@ Page({
         })
       }
     })
-  }
+  },
+
+  onReachBottom: function () {
+    var that = this
+    wx.request({
+      url: 'https://www.semidream.com/trophydata/?platForm=ps4', //仅为示例，并非真实的接口地址
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data)
+        that.setData({
+          list: res.data
+        })
+      }
+    })
+  },
 })
